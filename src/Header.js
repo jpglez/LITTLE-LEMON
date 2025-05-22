d68367829cb80c4e946330f299156fd7ce87cf69
Juan Pablo Gonzalez Sanmiguel <jp.gonzalezsanmiguel@gmail.com>
2025-05-22 12:25:04 -0600

    Progress of Little Lemon Restaurant

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,19 @@
 function Header() {
   return (
     <header>
-    <img src="/images/logo.png" alt="Little Lemon Logo" />
+    <div className="header-container">
+    <img src="/images/logo lemon.svg" alt="Little Lemon Logo" />
+     <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#reservations">Reservations</a></li>
+            <li><a href="#order">Order Online</a></li>
+            <li><a href="#login">Login</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,9 @@
 function Main() {
   return (
     <main>
-      <h2>Welcome to Little Lemon!</h2>
-      <p>Enjoy fresh Mediterranean cuisine delivered to your doorstep.</p>
+      <h2>Little Lemon</h2>
+      <h3>Chicago</h3>
+      <p>We are a family owned Mediterranean restaurant focused on traditional recipes served with a modern twist.</p>
     </main>
   );
 }
